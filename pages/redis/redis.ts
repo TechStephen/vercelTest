c11b2a5b8df18d53151bff50d56c78c68ff4ccf4
TechStephen <Stephenschmitz518@gmail.com>
2022-09-27 13:58:59 -0400

    initial commit pt2

--- pages/redis/redis.ts
+++ pages/redis/redis.ts
@@ -1,6 +1,6 @@
 import Redis, { RedisOptions } from 'ioredis';
 
 // need to be type RedisOptions cant find online??
-export const redis = new Redis(process.env.REDIS_URL)
+export const redis = new Redis('rediss://:7f4f848009e742bdac752ea434aa5e9e@us1-lasting-drake-38465.upstash.io:38465')
 
 
